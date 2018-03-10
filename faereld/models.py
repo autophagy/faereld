443ffcd3fae6c36993c0625949afb2a1e4fbffd0
Autophagy <mika@crate.io>
2018-03-10 23:41:05 +0100

    Add comments to models

--- faereld/models.py
+++ faereld/models.py
@@ -4,6 +4,15 @@
 faereld.models
 --------------
 
+This module models the data structure used within faereld, which consists of:
+AREA   :: The category of the time tracked
+OBJECT :: The specific object or project tracked
+START  :: The datetime the task was started
+END    :: The datetime the task was finished
+
+The START and END fields can either use the Gregorian datetime (by default),
+which returns the standard python datetime object. Or it can use the Wending
+datetime, which returns a Wending object as defined in Datarum.
 """
 
 from wisdomhord import Bisen, Sweor, String, Wending, DateTime
