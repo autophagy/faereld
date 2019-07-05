e2ac487421b56df3db97db06b61b88ef0fac5d92
Autophagy <mika@crate.io>
2019-07-05 13:57:51 +0200

    Release 0.1.2

--- faereld/__init__.py
+++ faereld/__init__.py
@@ -2,6 +2,6 @@
 __title__ = "faereld"
 __author__ = "Mika Naylor (Autophagy)"
 __license__ = "MIT"
-__version__ = (0, 1, 1)
+__version__ = (0, 1, 2)
 version = "%s.%s.%s" % __version__
 title = __title__
